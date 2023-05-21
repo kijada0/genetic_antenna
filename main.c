93568e342e9daced64ce5185a25d0259f698bfa4
kijada <188354e@gmail.com>
2023-05-21 22:39:34 +0200

    update

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
+// ---------------------------------------- //
+// created by: Kacper Kaczmarek
+// created date: 20-05-2023
+// ---------------------------------------- //
+
 #include <stdio.h>
+#include <math.h>
+
+#include <libnecpp.h>
 
 int main() {
-    printf("Hello, World!\n");
+    nec_context* nec;
+    nec = nec_create();
+    printf("nec: %p\n", nec);
+
     return 0;
 }
